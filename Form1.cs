cb2ed34156249705ced64f17cbdeca97a47a384d
Gergan Gospodinov <gospodinovgergan@gmail.com>
2024-08-14 18:38:09 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Forms;
+
+using Tekla.Structures.Drawing;
+using Point = Tekla.Structures.Geometry3d.Point;
+using PointList = Tekla.Structures.Drawing.PointList;
+
+namespace Revisions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+
+            var MyDrawingHandler = new DrawingHandler();
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                var views = MyDrawingHandler.GetActiveDrawing().GetSheet().GetAllViews();
+                while (views.MoveNext())
+                {
+                    Cloud cloud;
+                    var pointList = new PointList();
+                    pointList.Add(new Point(10, 10));
+                    pointList.Add(new Point(250, 10));
+                    pointList.Add(new Point(350, 450));
+                    pointList.Add(new Point(375, 675));
+                    cloud = new Cloud(views.Current as ViewBase, pointList);
+                    cloud.Attributes.Line.Color = DrawingColors.Cyan;
+                    cloud.Insert();
+                }
+            }
+
+            MyDrawingHandler.GetActiveDrawing().CommitChanges();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Tekla.Structures.Drawing;
+
+using Point = Tekla.Structures.Geometry3d.Point;
+using PointList = Tekla.Structures.Drawing.PointList;
+
+namespace Revisions
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            while (true)
+            {
+                try
+                {
+                    var dh = new DrawingHandler();
+
+                    var picker = dh.GetPicker();
+                    picker.PickTwoPoints("Pick insertion point", "Pick diagonal point", out var startPoint, out var endPoint, out var view);
+
+                    var pointList = new PointList();
+                    pointList.Add(new Point(startPoint.X, startPoint.Y));
+                    pointList.Add(new Point(endPoint.X, startPoint.Y));
+                    pointList.Add(new Point(endPoint.X, endPoint.Y));
+                    pointList.Add(new Point(startPoint.X, endPoint.Y));
+
+                    var cloud = new Cloud(view as ViewBase, pointList);
+                    cloud.Attributes.Line.Color = DrawingColors.Cyan;
+                    cloud.Bulge = 0.35;
+                    cloud.Insert();
+
+                    dh.GetActiveDrawing().CommitChanges();
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
