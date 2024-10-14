f5dafb9711e630aaf2f6deaeab4bb4c441fa37b9
Gergan Gospodinov <gospodinovgergan@gmail.com>
2024-10-14 14:30:11 +0100

    Revision Mark Added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Tekla.Structures.Drawing;
-
 using Point = Tekla.Structures.Geometry3d.Point;
 using PointList = Tekla.Structures.Drawing.PointList;
 
@@ -23,9 +22,14 @@ namespace Revisions
                 try
                 {
                     var dh = new DrawingHandler();
+                    var drawing = dh.GetActiveDrawing();
+                    var drawingType = drawing.GetType().ToString();
+                    var plotName = drawing.GetPlotFileName(true);
+                    var revIndex = plotName.LastIndexOf("_");
+                    var rev = plotName.Substring(revIndex + 1);
 
                     var picker = dh.GetPicker();
-                    picker.PickTwoPoints("Pick insertion point", "Pick diagonal point", out var startPoint, out var endPoint, out var view);
+                    picker.PickTwoPoints("Pick insertion point ...", "Pick diagonal point ...", out var startPoint, out var endPoint, out var view);
 
                     var pointList = new PointList();
                     pointList.Add(new Point(startPoint.X, startPoint.Y));
@@ -33,21 +37,22 @@ namespace Revisions
                     pointList.Add(new Point(endPoint.X, endPoint.Y));
                     pointList.Add(new Point(startPoint.X, endPoint.Y));
 
-                    var cloud = new Cloud(view as ViewBase, pointList);
+                    var cloud = new Cloud(view, pointList);
                     cloud.Attributes.Line.Color = DrawingColors.Cyan;
                     cloud.Bulge = 0.30;
                     cloud.Insert();
-
-                    //var offset = 50;
-                    //if (view.IsSheet) { offset = 5; }
-
-                    //var revisionPoint = new Point(endPoint.X + offset, endPoint.Y + offset);
-                    //var text = new Text(view as ViewBase, revisionPoint, "P01");
-                    //text.Attributes.Frame.Color = DrawingColors.Black;
-                    //text.Attributes.Frame.Type = FrameTypes.Triangle;
-                    //text.Insert();
-
                     dh.GetActiveDrawing().CommitChanges();
+
+                    picker.PickPoint("Pick revision insertion point ...", out var endPointRev, out var viewRev);
+
+                    var revisionPoint = new Point(endPointRev.X, endPointRev.Y);
+                    var text = new Text(view, revisionPoint, rev);
+                    text.Attributes.Frame.Color = DrawingColors.Black;
+                    text.Attributes.Frame.Type = FrameTypes.Triangle;
+                    text.Attributes.Font.Color = DrawingColors.Cyan;
+                    text.Insert();
+
+                    drawing.CommitChanges();
                 }
                 catch (Exception)
                 {
